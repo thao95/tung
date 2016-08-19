c06460838316e62153be0afc1f99c40773b330ad
tranquytung <tranquytung96@gmail.com>
2016-08-19 10:37:41 +0700

    bai tap ve nha

--- hoc-jquery/js/app.js
+++ hoc-jquery/js/app.js
@@ -19,7 +19,7 @@ alert(src);*/
 $('#demo1').mouseleave(function () {
      $('#demo1').attr('src','img/img1.png');
 });*/
-/$("#batden").attr('disabled', '');
+/*$("#batden").attr('disabled', '');
 $("#batden").click(function () {
      $("body").css('background','#fff');//học hàm
      $(this).attr('disabled', '');
@@ -29,8 +29,47 @@ $("#tatden").click(function () {
      $("body").css('background','black');
      $(this).attr('disabled', '');
      $("#batden").removeAttr('disabled');
-});
+});*/
+/*var time=6;
+$('h1').text(time);
+var demlui=setInterval(function () {
+     time--;
+     $('h1').text(time);
+     if(time==0)
+     {
+          clearInterval(demlui);
+          $('h1').text("bùm");
+     }
+},1000);*/
+/*var time=0;
+var t=0;
+$('#node2').text(time + "giây");
+setInterval(function () {
+     time++;
+     $('#node2').text(time + "giây");
+     if(time==59)
+     {
+          time=0;
+          $('#node2').text(time + "giây");
+          for(time=-1;time==60;time++)
+          {
+               $('#node2').text(time + "giây");
+          }
+          t++;
+          $('#node1').text(t + "phút");
+     }
+},1000);*/
+/*var laptop = [
+     {
+          "congty" : "Acer",
+          "giatien": "12 triệu",
+          "noisanxuat": "China"
+     },
+     {
+          "congty" : "Dell",
+          "giatien": "15 triệu",
+          "noisanxuat": "Mỹ"
+     }
 
-
-
+]*/
 
