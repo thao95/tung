70c97f14acc146fcef3d56fff42ede219d53b02f
tranquytung <tranquytung96@gmail.com>
2016-08-24 13:06:04 +0700

    hoc bootstap

--- hoc-jquery/js/tinh-giai-thua.js
+++ hoc-jquery/js/tinh-giai-thua.js
@@ -1,6 +1,7 @@
 /**
  * Created by AD on 8/22/2016.
  */
+
 function tinhgiaithua(n) {
     if(n <=1)
     {
@@ -9,11 +10,13 @@ function tinhgiaithua(n) {
     else{
         return n * tinhgiaithua(n-1);
     }
-
-$("#inp1").click(function () {
-    var n=$("#inp").val();
-    $('#p12').html("kết quả là  " + tinhgiaithua(n));
-});
+};
+$("#inp").keypress(function(event) {
+    if (event.which == 1){
+        var n=$("#inp").val();
+        $('#p12').html("kết quả là  " + tinhgiaithua(n));
+    }
+})
 
 
 
